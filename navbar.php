aaa001c1ca685fc40955a98d9f28dba67009f618
krsvtae <145859442+krsvtae@users.noreply.github.com>
2024-06-23 05:00:03 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,84 @@
+<nav class="navbar navbar-expand-lg nav-dark">
+    <div class="container">
+      <a class="navbar-brand" href="dashboardRoom.html"><img src="../Image/boardmeeting.png" width="60px" alt="eRooms Icon">
+        &nbsp;<span class="fs-4 text-white"><strong>eRooms</strong></span></a>
+       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav  me-auto mb-2 mb-lg-0">
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Master
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="./listDept.html">Department</a></li>
+              <li><a class="dropdown-item" href="./listRoom.html">Room</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Booking
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="./bookingRoom.html">Booking Room</a></li>
+              <li><a class="dropdown-item" href="./historyUser.html">History</a></li>
+              <li><a class="dropdown-item" href="./updateCancel.html">Update/Cancel</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false" data-bs-auto-close="outside">
+              Report
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbar-dropdown-link">
+              <li><a class="dropdown-item" href="./report.html">Monthly Department</a></li>
+              <li><a class="dropdown-item" href="./chart.html">Monthly Meeting Room</a></li>
+              <li><a class="dropdown-item" href="./historyDept.html">History Department</a></li>
+              <li><a class="dropdown-item" href="./incoming.html">Incoming Meeting</a></li>
+              <!-- <li class="dropdown dropend">
+                <a href="#" class="dropdown-item dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">Outstanding</a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbar-dropdown-link">
+                  <li><a href="#" class="dropdown-item">Overdue</a></li>
+                  <li><a href="#" class="dropdown-item">On Going</a></li>
+                  <li><a href="./report.html" class="dropdown-item">Report</a></li> -->
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false" data-bs-auto-close="outside">
+              Dashboard TV
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbar-dropdown-link">
+              <li><a class="dropdown-item" href="./dashboardTv.html">Dashboard TV</a></li>
+              <!-- <li class="dropdown dropend">
+                <a href="#" class="dropdown-item dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">Outstanding</a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbar-dropdown-link">
+                  <li><a href="#" class="dropdown-item">Overdue</a></li>
+                  <li><a href="#" class="dropdown-item">On Going</a></li>
+                  <li><a href="./report.html" class="dropdown-item">Report</a></li> -->
+            </ul>
+          </li>
+        </ul>
+        <div class="d-flex align-items-center">
+          <div style="position: relative;">
+            <a class="nav-link text-white" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Profile
+              <img src="../Image/profile.png" width="60px" alt="Gambar Profile" class="img-fluid navbar-brand" />
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="../changepass.html">Change Password</a></li>
+              <li><a class="dropdown-item" href="../update.html">Update Profile</a></li>
+              <li><a class="dropdown-item" href="../index.html">Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  
